9d8801e40193d65f0ddc28b9d17415d9cfb9e12d
Vraiment <jemc44@gmail.com>
2020-01-31 08:25:21 -0800

    Add ToStringMethod<T> delegate and builder

---
+++ NUtils/MethodBuilders/ToStringMethod.cs
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Vraiment
+ *
+ * This source code file is subject to the terms of the MIT license.
+ * If a copy of the MIT license was not distributed with this file,
+ * You can obtain one at https://opensource.org/licenses/MIT.
+ */
+using System;
+
+namespace NUtils.MethodBuilders
+{
+    /// <summary>
+    /// The signature of the method to generate strings representing
+    /// type <typeparamref name="T"/>.
+    /// </summary>
+    /// 
+    /// <param name="object">
+    /// The object to generate a string from.
+    /// </param>
+    /// 
+    /// <returns>
+    /// The string representing <param name="T"/>.
+    /// </returns>
+    public delegate string ToStringMethod<T>(T @object);
+
+    /// <summary>
+    /// Class to build a new build <see cref="ToStringMethod{T}"/> instances.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">
+    /// The type of objects that will be used to generate strings.
+    /// </typeparam>
+    public sealed class ToStringMethodBuilder<T>
+    {
+        /// <summary>
+        /// Builds a new <see cref="ToStringMethod{T}"/>.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// A new instance of <see cref="ToStringMethod{T}"/>.
+        /// </returns>
+        public ToStringMethod<T> Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
